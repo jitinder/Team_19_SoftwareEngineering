22b105bd5db9fb234db314f7597c7ac8a43a70a1
Sidak <sidak.pasrichassp@gmail.com>
2018-12-11 21:31:56 +0000

    Further Refactoring and Cleaning-up

--- src/com/trafficmon/ChargeCalculator.java
+++ src/com/trafficmon/ChargeCalculator.java
@@ -3,15 +3,20 @@ package com.trafficmon;
 import java.math.BigDecimal;
 import java.util.Calendar;
 
-public class ChargeCalculator {
+public abstract class ChargeCalculator {
 
     /* Constants */
-    private static final BigDecimal CHARGE_BEFORE_TWO_PM = new BigDecimal(6);
-    private static final BigDecimal CHARGE_AFTER_TWO_PM = new BigDecimal(4);
-    private static final int FOUR_HOURS_IN_MINS = 4 * 60;
+    public final BigDecimal CHARGE_BEFORE_TWO_PM = new BigDecimal(6);
+    public final BigDecimal CHARGE_AFTER_TWO_PM = new BigDecimal(4);
+    public final BigDecimal CHARGE_OVERTIME = new BigDecimal(12);
+    public final int ONE_HOUR_IN_MS = 60 * 60 * 1000;
+    public final int FOUR_HOURS_IN_MINS = 4 * 60;
 
+
+    /* Time Calculation Methods */
     /**
      * Calculates the number of minutes between the startTime and endTime
+     *
      * @param startTimeMs is start time in milliseconds
      * @param endTimeMs is end time in milliseconds
      * @return number of minutes passed as an integer
@@ -22,10 +27,11 @@ public class ChargeCalculator {
 
     /**
      * Checks whether the driver should be charged again by checking whether their last entry was four hours ago
+     *
      * @param firstEntryTimeMs is the first time of entry in milliseconds
      * @param currentTime is the current time (of re-entry) in milliseconds
      * @return true if user should be charged again
-     * @return false if user should not be charged again
+     *         false if user should not be charged again
      */
     public boolean canChargeAgain(long firstEntryTimeMs, long currentTime) {
         int timeTaken = getMinutesBetween(firstEntryTimeMs, currentTime);
@@ -37,6 +43,7 @@ public class ChargeCalculator {
 
     /**
      * Calculates the {@link EntryEvent} charge based on the timestamp by comparing it with 2 pm of the same day.
+     *
      * @param timestamp is the timestamp the {@link EntryEvent} took place at
      * @return a BigDecimal value based on whether the {@link EntryEvent} occurred before or after 2 pm.
      */
--- src/com/trafficmon/CongestionChargeSystem.java
+++ src/com/trafficmon/CongestionChargeSystem.java
@@ -6,225 +6,44 @@ import java.util.*;
 public class CongestionChargeSystem implements TransactionHandler{
 
     /* Constants */
-    private static final BigDecimal CHARGE_BEFORE_TWO_PM = new BigDecimal(6);
-    private static final BigDecimal CHARGE_AFTER_TWO_PM = new BigDecimal(4);
-    private static final BigDecimal CHARGE_OVERTIME = new BigDecimal(12);
-    private static final int ONE_HOUR_IN_MS = 60 * 60 * 1000;
-    private static final int FOUR_HOURS_IN_MINS = 4 * 60;
-    private final List<ZoneBoundaryCrossing> eventLog = new ArrayList<ZoneBoundaryCrossing>();
-    private static AccountsService accountsService = RegisteredCustomerAccountsService.getInstance();
-    private static PenaltiesService penaltiesService = OperationsTeam.getInstance();
+    private AccountsService accountsService = RegisteredCustomerAccountsService.getInstance();
+    private PenaltiesService penaltiesService = OperationsTeam.getInstance();
+    private CongestionChargeCalculator chargeCalculator = new CongestionChargeCalculator();
 
     /* Singleton Class code */
     private static CongestionChargeSystem instance = new CongestionChargeSystem();
 
     /* Getters and Setters */
+    /**
+     * Instantiates the {@link CongestionChargeSystem} at runtime
+     *
+     * @return the instance of the class
+     */
     public static CongestionChargeSystem getInstance() {
         return instance;
     }
-    public static void setAccountsService(AccountsService newAccountsService){
+
+    /**
+     * Used to set a custom {@link AccountsService} for this class
+     *
+     * @param newAccountsService is a valid {@link AccountsService} instance
+     */
+    public void setAccountsService(AccountsService newAccountsService){
         accountsService = newAccountsService;
     }
-    public static void setPenaltiesService(PenaltiesService newPenaltiesService){
+
+    /**
+     * Used to set a custom {@link PenaltiesService} for this class
+     * @param newPenaltiesService is a valid {@link PenaltiesService} instance
+     */
+    public void setPenaltiesService(PenaltiesService newPenaltiesService){
         penaltiesService = newPenaltiesService;
     }
-    public List<ZoneBoundaryCrossing> getEventLog() {
-        return eventLog;
-    }
 
-    /* Vehicle Data Methods */
-    /**
-     * Marks the Vehicle's entry in the zone by storing a {@link EntryEvent} for the Vehicle in the eventLog List
-     *
-     * @param vehicle is a {@link Vehicle} object
-     */
-    public void vehicleEnteringZone(Vehicle vehicle) {
-        eventLog.add(new EntryEvent(vehicle));
-    }
-
-    /**
-     * Marks the Vehicle's exit from the zone by storing a {@link ExitEvent} for the Vehicle in the eventLog List
-     * If any previous Vehicle event is not yet registered in the system, doesn't add the {@link ExitEvent}.
-     *
-     * @param vehicle is a {@link Vehicle} object
-     */
-    public void vehicleLeavingZone(Vehicle vehicle) {
-        if (!previouslyRegistered(vehicle)) {
-            return;
-        }
-        eventLog.add(new ExitEvent(vehicle));
-    }
-
-    /**
-     * Checks whether a previous EventEntry exists in eventLog for the Vehicle entered
-     *
-     * @param vehicle is a {@link Vehicle} object
-     * @return true if a {@link ZoneBoundaryCrossing} event has been previously registered for the event
-     * @return false if a {@link ZoneBoundaryCrossing} event has not been previously registered for the event
-     */
-    private boolean previouslyRegistered(Vehicle vehicle) {
-        for (ZoneBoundaryCrossing crossing : eventLog) {
-            if (crossing.getVehicle().equals(vehicle)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Checks the order of the Events added to the List of {@link ZoneBoundaryCrossing}
-     *
-     * @param crossings is a List of {@link ZoneBoundaryCrossing}
-     * @return true if the order of entries is valid
-     * @return false if the order of entries is not appropriate
-     */
-    private boolean checkOrderingOf(List<ZoneBoundaryCrossing> crossings) {
-
-        ZoneBoundaryCrossing lastEvent = crossings.get(0);
-
-        for (ZoneBoundaryCrossing crossing : crossings.subList(1, crossings.size())) {
-            if (crossing.timestamp() < lastEvent.timestamp()) {
-                return false;
-            }
-            if (crossing instanceof EntryEvent && lastEvent instanceof EntryEvent) {
-                return false;
-            }
-            if (crossing instanceof ExitEvent && lastEvent instanceof ExitEvent) {
-                return false;
-            }
-            lastEvent = crossing;
-        }
-
-        return true;
-    }
-
-    /**
-     * Makes a Map of crossings per Vehicle from the eventLog
-     *
-     * @return a Map of Vehicle to List of all crossings in the eventLog per day.
-     */
-    private Map<Vehicle, List<ZoneBoundaryCrossing>> getCrossingsPerVehicle(){
-        HashMap<Vehicle, List<ZoneBoundaryCrossing>> crossingsPerVehicle = new HashMap<>();
-
-        for(ZoneBoundaryCrossing crossing: eventLog){
-            Vehicle currentVehicle = crossing.getVehicle();
-            if(!crossingsPerVehicle.containsKey(currentVehicle)){
-                crossingsPerVehicle.put(currentVehicle, new ArrayList<ZoneBoundaryCrossing>());
-            }
-            crossingsPerVehicle.get(currentVehicle).add(crossing);
-        }
-
-        return crossingsPerVehicle;
-    }
-
-    /* Charge and Time Calculation Methods */
-
-    /**
-     * Calculates the number of minutes between the startTime and endTime
-     * @param startTimeMs is start time in milliseconds
-     * @param endTimeMs is end time in milliseconds
-     * @return number of minutes passed as an integer
-     */
-    private int getMinutesBetween(long startTimeMs, long endTimeMs) {
-        return (int) Math.abs(Math.ceil((endTimeMs - startTimeMs) / (1000.0 * 60.0)));
-    }
-
-    /**
-     * Checks whether the driver should be charged again by checking whether their last entry was four hours ago
-     * @param firstEntryTimeMs is the first time of entry in milliseconds
-     * @param currentTime is the current time (of re-entry) in milliseconds
-     * @return true if user should be charged again
-     * @return false if user should not be charged again
-     */
-    private boolean canChargeAgain(long firstEntryTimeMs, long currentTime) {
-        int timeTaken = getMinutesBetween(firstEntryTimeMs, currentTime);
-        if(timeTaken <= FOUR_HOURS_IN_MINS){
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Calculates the {@link EntryEvent} charge based on the timestamp by comparing it with 2 pm of the same day.
-     * @param timestamp is the timestamp the {@link EntryEvent} took place at
-     * @return a BigDecimal value based on whether the {@link EntryEvent} occurred before or after 2 pm.
-     */
-    private BigDecimal getChargeByTime(long timestamp){
-        Calendar calendarForTwoPm = Calendar.getInstance();
-        Calendar calendarForTimestamp = Calendar.getInstance();
-        calendarForTwoPm.set(Calendar.HOUR_OF_DAY, 14);
-        calendarForTwoPm.set(Calendar.MINUTE, 0);
-        calendarForTimestamp.setTimeInMillis(timestamp);
-
-        if(calendarForTimestamp.after(calendarForTwoPm)) {
-            return CHARGE_AFTER_TWO_PM;
-        }
-        return CHARGE_BEFORE_TWO_PM;
-    }
-
-
-    /* Charge Calculation and Returning Methods*/
-    /**
-     * Calculates the overall charge to all vehicles in the eventLog over the whole day considering all their {@link ZoneBoundaryCrossing}
-     * activities logged in the list.
-     * @return a {@link HashMap} with the Vehicle as a key and {@link BigDecimal} containing the total money to charge the vehicle
-     * based on the day's activity
-     */
-    public Map<Vehicle, BigDecimal> getChargeList(){
-        Map<Vehicle,  List<ZoneBoundaryCrossing>> crossingsPerVehicle = getCrossingsPerVehicle();
-        Map<Vehicle, BigDecimal> chargePerVehicle = new HashMap<>();
-
-        for(Vehicle vehicle: crossingsPerVehicle.keySet()){
-
-            BigDecimal vehicleCharge = new BigDecimal(0);
-            List<ZoneBoundaryCrossing> crossingsByVehicle = crossingsPerVehicle.get(vehicle);
-
-            if (!checkOrderingOf(crossingsByVehicle)) {
-                OperationsTeam.getInstance().triggerInvestigationInto(vehicle);
-            } else {
-                EntryEvent lastChargedEntry = (EntryEvent) crossingsByVehicle.get(0);
-                vehicleCharge = vehicleCharge.add(getChargeByTime(lastChargedEntry.timestamp()));
-
-                for(ZoneBoundaryCrossing crossing : crossingsByVehicle.subList(1, crossingsByVehicle.size())){
-                    if(crossing instanceof ExitEvent){
-                        int durationInArea = getMinutesBetween(lastChargedEntry.timestamp(), crossing.timestamp());
-                        if(durationInArea > FOUR_HOURS_IN_MINS){
-                            vehicleCharge = CHARGE_OVERTIME;
-                            break;
-                        }
-                    }
-
-                    if(crossing instanceof EntryEvent){
-                        if(canChargeAgain(lastChargedEntry.timestamp(), crossing.timestamp())) {
-                            vehicleCharge = vehicleCharge.add(getChargeByTime(crossing.timestamp()));
-                            lastChargedEntry = (EntryEvent) crossing;
-                        }
-                    }
-                }
-            }
-            chargePerVehicle.put(vehicle, vehicleCharge);
-        }
-
-        return chargePerVehicle;
-    }
-
-    /**
-     * Gives the overall charge for a specified vehicle based on its activity in the eventLog.
-     * @param vehicle is the Vehicle whose charge is being determined
-     * @return a {@link BigDecimal} object with the charge or 0 value if the vehicle isn't in the eventLog
-     */
-    public BigDecimal getChargeForVehicle(Vehicle vehicle){
-        Map<Vehicle, BigDecimal> chargeList = getChargeList();
-        if(chargeList.keySet().contains(vehicle)) {
-            return chargeList.get(vehicle);
-        }
-        return new BigDecimal(0);
-    }
-
-
-    /* Account Manipulation / ChargeSystem Methods */
+    /* Transaction Methods */
     /**
      * * Charges the Vehicle owner the specified amount of money
+     *
      * @param vehicle is the {@link Vehicle} associated with an {@link Account}
      * @param charge is the {@link BigDecimal} value to charge the {@link Account}
      * @param accountsService refers to the appropriate service for handling accounts - {@link RegisteredCustomerAccountsService} in this case.
@@ -236,20 +55,33 @@ public class CongestionChargeSystem implements TransactionHandler{
     }
 
     /**
-     * Handles all transactions and handles any errors thrown by their processes. Overrides the method from {@link TransactionHandler}.
-     * Uses the static {@link AccountsService} and {@link PenaltiesService} in the system
+     * Executes a particular transaction and handles any errors thrown by the process. Overrides the method from {@link TransactionHandler}.
+     * Uses the {@link AccountsService} and {@link PenaltiesService} designated to the class
+     *
      * @param object refers to the {@link Vehicle} in this case.
+     * @param charge refers to the {@link BigDecimal} charge.
      */
     @Override
-    public void executeTransaction(Object object){
+    public void executeTransactionFor(Object object, BigDecimal charge) {
         Vehicle vehicle = (Vehicle) object;
-        BigDecimal charge = getChargeForVehicle(vehicle);
         try {
             chargeVehicle(vehicle, charge, accountsService);
         } catch (AccountNotRegisteredException e) {
             penaltiesService.issuePenaltyNotice(vehicle, charge);
         } catch (InsufficientCreditException e) {
             penaltiesService.issuePenaltyNotice(vehicle, charge);
+        }
+    }
+
+    /**
+     * Executes all transactions for the day's activity using the {@link ChargeCalculator} which returns a list of all vehicles and their respective charges from {@link EventLog}.
+     * Overrides the method from {@link TransactionHandler}.
+     */
+    @Override
+    public void executeAllTransactions() {
+        Map<Vehicle, BigDecimal> chargeList = chargeCalculator.getChargeList();
+        for(Vehicle vehicle : chargeList.keySet()){
+            executeTransactionFor(vehicle, chargeList.get(vehicle));
         }
     }
 
